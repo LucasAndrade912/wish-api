4188244f1f0f052ff6a7c2baf45de7665247cd82
Lucas Andrade <lucasantos.dev@gmail.com>
2025-09-22 21:22:03 -0300

    feat: enhance DomainNotSupportedException message to include supported domains

--- src/exceptions/domainNotSupportedException.ts
+++ src/exceptions/domainNotSupportedException.ts
@@ -1,7 +1,8 @@
 export class DomainNotSupportedException extends Error {
     constructor() {
-        super('Domain not supported for scraping');
+        super('Domain not supported for scraping. Supported domains: pt.aliexpress.com');
         this.name = 'DomainNotSupportedException';
-        this.message = 'Domain not supported for scraping';
+        this.message =
+            'Domain not supported for scraping. Supported domains: pt.aliexpress.com';
     }
 }
